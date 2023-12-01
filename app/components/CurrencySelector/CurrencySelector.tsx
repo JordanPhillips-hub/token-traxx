fc4902d8353c66e958d0b2e2aa297b736ed70df7
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:39:35 -0600

    Create CurrencySelector

---
+++ app/components/CurrencySelector/CurrencySelector.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import PrimaryButton from "../Buttons/PrimaryButton";
+import PriceChange from "../PriceChange/PriceChange";
+
+type CurrencySelectorProps = {
+  img: string;
+  name: string;
+  price: string;
+  percentage: number;
+  onClick: () => void;
+};
+
+export default function CurrencySelector({
+  img,
+  name,
+  price,
+  percentage,
+  onClick,
+}: CurrencySelectorProps) {
+  return (
+    <div className="max-w-[250px]">
+      <PrimaryButton size="xl" onClick={onClick}>
+        <div className="flex items-center gap-4 pointer-events-none">
+          <div>
+            <Image src={img} alt="Coin Icon" width={32} height={32} />
+          </div>
+
+          <div className="text-sm flex-col">
+            <p className="font-medium text-left">{name}</p>
+            <div className="flex items-center gap-2">
+              <p className="text-left">{price} USD</p>
+              <PriceChange percentage={percentage} />
+            </div>
+          </div>
+        </div>
+      </PrimaryButton>
+    </div>
+  );
+}

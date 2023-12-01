fa5a96278fae0963a5f9c2dcf65bfceedfb12a6a
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:53:06 -0600

    Create TimePeriodSelector

---
+++ app/components/TimePeriodSelector/TimePeriodSelector.tsx
@@ -0,0 +1,27 @@
+import PeriodToggleButton from "../Buttons/PeriodToggleButton";
+
+type TimePeriodSelectorProps = {
+  onTimePeriodChange: (timePeriod: number) => void;
+};
+
+const timePeriods = ["1D", "7D", "14D", "1M", "3M"];
+
+export default function TimePeriodSelector({
+  onTimePeriodChange,
+}: TimePeriodSelectorProps) {
+  const handleClick = (timePeriod: number) => {
+    onTimePeriodChange(timePeriod);
+  };
+
+  return (
+    <div className="bg-primary700 flex w-fit mt-10 rounded-md">
+      {timePeriods.map((timePeriod) => (
+        <PeriodToggleButton
+          key={timePeriod}
+          text={timePeriod}
+          onClick={handleClick}
+        />
+      ))}
+    </div>
+  );
+}

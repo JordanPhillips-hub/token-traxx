a08855c1a8649223d2cfb7167aa975e7fbae5456
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:44:59 -0600

    Create FormInput

---
+++ app/components/Inputs/FormInput/FormInput.tsx
@@ -0,0 +1,28 @@
+type FormInputProps = {
+  label: string;
+} & React.ComponentProps<"input">;
+
+export default function FormInput({
+  label,
+  id,
+  type,
+  name,
+  placeholder,
+  onChange,
+}: FormInputProps) {
+  return (
+    <>
+      <label className="hidden" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className="dark:bg-primary800 placeholder:text-sm py-3 pl-10 pr-4 rounded-md focused-primary"
+        id={id}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </>
+  );
+}

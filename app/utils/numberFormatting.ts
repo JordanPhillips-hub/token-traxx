64b4460a7a983f0c5f50f8d69dd9a54dcd56ab79
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:57:12 -0600

    Create numberFormatting util

---
+++ app/utils/numberFormatting.ts
@@ -0,0 +1,41 @@
+export function timestampToDate(timestamp: string | number | Date) {
+  const date = new Date(timestamp);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString(undefined, options);
+};
+
+export function formatPrice(price: number | undefined) {
+  if (price === undefined) {
+    return "N/A";
+  };
+  const formattedPrice = price.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return formattedPrice;
+};
+
+export function formatNum3_2(num: number) {
+  let baseNum = Math.floor(num);
+  let decimal = num - baseNum;
+  let formattedNum =
+    baseNum.toString().slice(0, 3) +
+    (decimal > 0 ? "." + decimal.toString().slice(2, 4) : "");
+  return formattedNum;
+};
+
+export function checkNumberScale(num: number) {
+  if (num >= 1e12) {
+    return "T";
+  } else if (num >= 1e9) {
+    return "B";
+  } else if (num >= 1e6) {
+    return "M";
+  } else {
+    return "";
+  };
+};

38a9916439bf0083c8eefd4450c8f4a5b4e5b550
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 13:06:54 -0600

    Create themeProvider

---
+++ app/themeProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+import { ThemeProvider } from "next-themes";
+
+export default function Theme({ children }: { children: any }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}

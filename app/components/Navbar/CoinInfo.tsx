0a2a987b7950936a4b91044b5e801906f4d4d13e
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:48:47 -0600

    Create CoinInfo

---
+++ app/components/Navbar/CoinInfo.tsx
@@ -0,0 +1,54 @@
+import ProgressBar from "@ramonak/react-progress-bar";
+import Icon from "../Icon/Icon";
+import PriceChange from "../PriceChange/PriceChange";
+
+type CoinInfoProps = {
+  icon?: string;
+  name?: string;
+  data: number | string;
+  hasProgressBar?: boolean;
+  completed: number | string;
+  hasPriceChange?: boolean;
+  changePercent: number;
+};
+
+export default function CoinInfo({
+  icon,
+  name,
+  data,
+  hasProgressBar,
+  completed,
+  hasPriceChange,
+  changePercent,
+}: CoinInfoProps) {
+  return (
+    <>
+      {hasPriceChange ? (
+        <div className="text-xs">
+          <PriceChange percentage={changePercent} />
+        </div>
+      ) : (
+        <div className="font-medium flex items-center gap-1">
+          <div className="flex items-center gap-1">
+            {icon && <Icon iconVariant={icon} />}
+            <div className="text-xs">
+              <span className="text-neutral400 mr-2">{name}</span>
+              <span>{data}</span>
+            </div>
+          </div>
+
+          {hasProgressBar && (
+            <ProgressBar
+              completed={completed}
+              animateOnRender={true}
+              labelColor="transparent"
+              baseBgColor="hsla(0, 0%, 100%, 0.5)"
+              bgColor={`${name === "BTC" ? "#F7931A" : "#7D9EFF"}`}
+              height="5px"
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+}

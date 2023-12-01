ccd44ebe462473cf977460285d341f7887f792bd
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-01 12:46:06 -0600

    Create PageLink

---
+++ app/components/Links/PageLink/PageLink.tsx
@@ -0,0 +1,42 @@
+import { useRef, useEffect } from "react";
+import Link from "next/link";
+
+type PageLinkProps = {
+  text?: string;
+  href: string;
+  id?: string;
+  children?: React.ReactNode;
+} & React.ComponentProps<"a">;
+
+export default function PageLink({
+  text,
+  href,
+  id,
+  children,
+  onClick,
+}: PageLinkProps) {
+  const linkRef = useRef<HTMLAnchorElement | null>(null);
+
+  useEffect(() => {
+    const focusLinkId =
+      window.location.pathname === "/" ? "coinsLink" : "portfolioLink";
+    if (linkRef.current) {
+      if (linkRef.current.id === id && linkRef.current.id === focusLinkId) {
+        linkRef.current.focus();
+      }
+    }
+  }, [id]);
+
+  return (
+    <Link
+      id={id}
+      ref={linkRef}
+      className="text-neutral50 flex gap-2 items-center justify-center outline-none hover:text-neutral100 focus:text-neutral100"
+      href={href}
+      onClick={onClick}
+    >
+      {children}
+      {text}
+    </Link>
+  );
+}

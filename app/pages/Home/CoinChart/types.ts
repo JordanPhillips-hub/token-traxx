f56f62644a1ae5d6bcb5842f8adbab4a96bef278
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-02 13:27:55 -0600

    Add chart container types

--- app/pages/Home/CoinChart/types.ts
+++ app/pages/Home/CoinChart/types.ts
@@ -1,3 +1,11 @@
+export type ChartContainerProps = {
+  children: React.ReactNode;
+  name: string;
+  price: number | string;
+  date: string;
+  type: string;
+};
+
 export type ChartProps = {
   chartType: string;
   coinId: string;

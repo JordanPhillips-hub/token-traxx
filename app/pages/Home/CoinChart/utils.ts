29a68e0a5067b70223b968a79631e2e426dc0c18
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-02 13:34:38 -0600

    Create utils file for CoinChart

---
+++ app/pages/Home/CoinChart/utils.ts
@@ -0,0 +1,24 @@
+import { formatPrice } from "@/app/utils/numberFormatting";
+import { optionalCapitalize } from "@/app/utils/optionalCapitalize";
+
+export function createGraphLabel(
+  comparedCoins: string[],
+  len: number,
+  coinId: string,
+  coins: any[],
+  priceType: "current_price" | "total_volume",
+  unit: string
+) {
+  const isGreaterThan = comparedCoins.length >= len;
+  const coinName = optionalCapitalize(coinId);
+  const findCoin = coins.find((coin) => coin.id === coinId);
+  const currentPrice = formatPrice(findCoin?.[priceType]);
+  return isGreaterThan ? `${coinName} $${currentPrice} ${unit}` : "";
+}
+
+export function createAxisLabel(arr: number[]) {
+  const label = arr.map((date: string | number | Date) =>
+    new Date(date).getDate()
+  );
+  return label;
+}

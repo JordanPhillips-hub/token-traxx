37a8c8b7c8c91b3746bf38ca73dff0bd28c06521
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-07 10:02:05 -0600

    Update coin market slice
    Updated the slice to set error and loading states

--- app/store/features/coinMarketSlice.ts
+++ app/store/features/coinMarketSlice.ts
@@ -2,26 +2,41 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type CoinMarketState = {
   coins: any[];
+  isMarketsLoading: boolean;
+  marketsHasError: boolean;
   coinId: string;
+  coinPage: number;
 };
 
 const initialState: CoinMarketState = {
   coins: [],
+  isMarketsLoading: false,
+  marketsHasError: false,
   coinId: 'bitcoin',
+  coinPage: 2,
 };
 
 const coinMarketSlice = createSlice({
   name: 'coins',
   initialState,
   reducers: {
-    setCoinMarkets(state, action: PayloadAction<[]>) {
+    setCoinMarkets(state, action: PayloadAction<any[]>) {
       state.coins = action.payload
     },
     setCoinId(state, action: PayloadAction<string>) {
       state.coinId = action.payload
+    },
+    setCoinPage(state, action: PayloadAction<number>) {
+      state.coinPage = action.payload
+    },
+    setIsMarketsLoading(state, action: PayloadAction<boolean>) {
+      state.isMarketsLoading = action.payload
+    },
+    setMarketsHasError(state, action: PayloadAction<boolean>) {
+      state.marketsHasError = action.payload
     }
   }
 });
 
-export const { setCoinMarkets, setCoinId } = coinMarketSlice.actions;
+export const { setCoinMarkets, setIsMarketsLoading, setMarketsHasError, setCoinId, setCoinPage } = coinMarketSlice.actions;
 export default coinMarketSlice.reducer;

3ef5311ea25ef9a852a0ed73cc6371d1a77c0e02
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-07 09:56:14 -0600

    Setup infinite scroll

--- app/pages/Home/CoinTable.tsx
+++ app/pages/Home/CoinTable.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Sparkline from "./Sparkline";
 import { formatCoinName } from "./utils";
-import { useAppSelector } from "@/app/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import StatusBar from "@/app/components/UI/StatusBar";
 import PriceChange from "@/app/components/UI/PriceChange";
 import { formatPrice } from "@/app/utils/numberFormatting";
+import { useGetMarketsQuery } from "@/app/store/api/coingecko";
+import {
+  setCoinMarkets,
+  setCoinPage,
+} from "@/app/store/features/coinMarketSlice";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const tableHeaders = [
   "#",
@@ -36,57 +42,84 @@ function createStatusBar(data1: number, data2: number) {
 }
 
 export default function CoinTable() {
-  const { coins } = useAppSelector((state) => state.coinMarkets);
+  const dispatch = useAppDispatch();
+  let { coins, coinPage } = useAppSelector((state) => state.coinMarkets);
+  const { data: updatedCoins, isError } = useGetMarketsQuery({
+    page: coinPage,
+  });
 
   return (
-    <table className="min-w-full border-separate border-spacing-y-2 text-sm">
-      <thead className="text-left text-sm text-neutral400">
-        <tr>
-          {tableHeaders.map((header, index) => (
-            <th className="pl-3" key={index}>
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {coins.map(
-          (
-            {
-              id,
-              image,
-              symbol,
-              current_price,
-              price_change_percentage_1h_in_currency: changeIn1h,
-              price_change_percentage_24h_in_currency: changeIn24h,
-              price_change_percentage_7d_in_currency: changeIn7d,
-              total_volume,
-              market_cap,
-              circulating_supply,
-              total_supply,
-              sparkline_in_7d: sparkline,
-            },
-            index
-          ) => (
-            <tr className="bg-primary800 rounded-2xl p-3 " key={id}>
-              <td className="py-2 pl-3">{index + 1}</td>
-              <td className="flex gap-2 mt-1.5">
-                <Image src={image} alt={`${id} icon`} width={32} height={32} />
-                <button>{formatCoinName(id, symbol)}</button>
-              </td>
-              <td>{`$${formatPrice(current_price)}`}</td>
-              <td>{createPriceChange(changeIn1h)}</td>
-              <td>{createPriceChange(changeIn24h)}</td>
-              <td>{createPriceChange(changeIn7d)}</td>
-              <td>{createStatusBar(total_volume, market_cap)}</td>
-              <td>{createStatusBar(circulating_supply, total_supply)}</td>
-              <td className="max-w-[200px]">
-                <Sparkline sparklinePrice={sparkline.price} />
-              </td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+    <InfiniteScroll
+      dataLength={coins.length}
+      next={() => {
+        if (updatedCoins && !isError) {
+          setTimeout(() => {
+            dispatch(setCoinPage(coinPage + 1));
+            console.log(updatedCoins);
+            dispatch(setCoinMarkets(coins.concat(updatedCoins)));
+          }, 1500);
+        }
+      }}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      <table className="min-w-full border-separate border-spacing-y-2 text-sm">
+        <thead className="text-left text-sm text-neutral400">
+          <tr>
+            {tableHeaders.map((header, index) => (
+              <th className="pl-3" key={index}>
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map(
+            (
+              {
+                id,
+                image,
+                symbol,
+                current_price,
+                price_change_percentage_1h_in_currency: changeIn1h,
+                price_change_percentage_24h_in_currency: changeIn24h,
+                price_change_percentage_7d_in_currency: changeIn7d,
+                total_volume,
+                market_cap,
+                circulating_supply,
+                total_supply,
+                sparkline_in_7d: sparkline,
+              },
+              index
+            ) => (
+              <tr
+                className="bg-primary800 rounded-2xl p-3 "
+                key={Math.random()}
+              >
+                <td className="py-2 pl-3">{index + 1}</td>
+                <td className="flex gap-2 mt-1.5">
+                  <Image
+                    src={image}
+                    alt={`${id} icon`}
+                    width={32}
+                    height={32}
+                  />
+                  <button>{formatCoinName(id, symbol)}</button>
+                </td>
+                <td>{`$${formatPrice(current_price)}`}</td>
+                <td>{createPriceChange(changeIn1h)}</td>
+                <td>{createPriceChange(changeIn24h)}</td>
+                <td>{createPriceChange(changeIn7d)}</td>
+                <td>{createStatusBar(total_volume, market_cap)}</td>
+                <td>{createStatusBar(circulating_supply, total_supply)}</td>
+                <td className="max-w-[200px]">
+                  <Sparkline sparklinePrice={sparkline.price} />
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </InfiniteScroll>
   );
 }

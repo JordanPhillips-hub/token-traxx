8d343394a8c62d79c5ad7341d3b393859ac23e8c
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-12 10:32:32 -0600

    Refactor page
    Refactored the page to include is modal open conditional rendering

--- app/pages/Home/page.tsx
+++ app/pages/Home/page.tsx
@@ -65,76 +65,91 @@ export default function Home() {
   ];
 
   return (
-    <main className="bg-grey100 dark:bg-slate700 max-w-8xl mx-auto px-24">
-      <section className="container mx-auto">
-        <div className="bg-primary800 text-1xl flex w-1/3 mb-10 py-1 px-1 rounded-md">
-          <CoinConvertorButton
-            isModalOpen={isModalOpen}
-            text="Coins"
-            activeStyles="Coins"
-          />
-          <CoinConvertorButton
-            isModalOpen={isModalOpen}
-            text="Convertor"
-            activeStyles="Convertor"
-            onClick={() => setIsModalOpen(true)}
-          />
-        </div>
-      </section>
-
-      <section className=" container relative mx-auto mb-4">
-        <div className="flex mb-6">
-          <div>
-            <PrimaryButton
-              size="med"
-              onClick={() => dispatch(setIsComparing(!isComparing))}
-            >
-              <div className="text-sm flex items-center gap-2">
-                <Icon iconVariant={isComparing ? "exit" : "graph"} />
-                {isComparing ? "Exit Comparison" : "Compare"}
-              </div>
-            </PrimaryButton>
+    <>
+      <main className="bg-grey100 dark:bg-slate700 max-w-8xl mx-auto px-24">
+        <section className="container mx-auto">
+          <div className="bg-primary800 text-1xl flex w-1/3 mb-10 py-1 px-1 rounded-md">
+            <CoinConvertorButton
+              isModalOpen={isModalOpen}
+              text="Coins"
+              activeStyles="Coins"
+            />
+            <CoinConvertorButton
+              isModalOpen={isModalOpen}
+              text="Convertor"
+              activeStyles="Convertor"
+              onClick={() => setIsModalOpen(true)}
+            />
           </div>
-        </div>
+        </section>
 
-        <CurrencySelector />
-      </section>
+        {isModalOpen ? (
+          <aside>
+            <CurrencyConvertorModal
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+            />
+          </aside>
+        ) : (
+          <>
+            <section className="container relative mx-auto mb-4">
+              <div className="flex mb-6">
+                <div>
+                  <PrimaryButton
+                    size="med"
+                    onClick={() => dispatch(setIsComparing(!isComparing))}
+                  >
+                    <div className="text-sm flex items-center gap-2">
+                      <Icon iconVariant={isComparing ? "exit" : "graph"} />
+                      {isComparing ? "Exit Comparison" : "Compare"}
+                    </div>
+                  </PrimaryButton>
+                </div>
+              </div>
+              <CurrencySelector />
+            </section>
 
-      <section className=" container flex-col mb-16 mx-auto">
-        <div className="flex gap-8">
-          {charts.map((chart) => {
-            const { name, value, date, type } = chart;
-            return (
-              <ChartContainer
-                key={name}
-                name={isLoading ? "Loading" : isError ? "Error Occurred" : name}
-                price={isLoading || isError ? "" : `$${value}`}
-                date={date}
-                type={type}
-              >
-                <CoinChart chartType={type} coinId={coinId} days={timePeriod} />
-              </ChartContainer>
-            );
-          })}
-        </div>
+            <section className=" container flex-col mb-16 mx-auto">
+              <div className="flex gap-8">
+                {charts.map((chart) => {
+                  const { name, value, date, type } = chart;
+                  return (
+                    <ChartContainer
+                      key={name}
+                      name={
+                        isLoading
+                          ? "Loading"
+                          : isError
+                          ? "Error Occurred"
+                          : name
+                      }
+                      price={isLoading || isError ? "" : `$${value}`}
+                      date={date}
+                      type={type}
+                    >
+                      <CoinChart
+                        chartType={type}
+                        coinId={coinId}
+                        days={timePeriod}
+                      />
+                    </ChartContainer>
+                  );
+                })}
+              </div>
 
-        <TimePeriodSelector
-          onTimePeriodChange={(timePeriod: number) =>
-            dispatch(setChartTimePeriod(timePeriod))
-          }
-        />
-      </section>
+              <TimePeriodSelector
+                onTimePeriodChange={(timePeriod: number) =>
+                  dispatch(setChartTimePeriod(timePeriod))
+                }
+              />
+            </section>
 
-      <section className="container mx-auto">
-        <CoinTable />
-      </section>
-
-      <aside>
-        <CurrencyConvertorModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-      </aside>
-    </main>
+            <section className="container mx-auto">
+              <CoinTable />
+            </section>
+          </>
+        )}
+      </main>
+    </>
   );
 }

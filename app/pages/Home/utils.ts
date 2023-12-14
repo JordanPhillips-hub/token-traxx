c0d327d4161af85b452f64a009705be59bd14e6b
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-14 17:16:01 -0600

    Delete files

--- app/pages/Home/utils.ts
+++
@@ -1,6 +0,0 @@
-import { formatPrice } from "@/app/utils/numberFormatting";
-import { optionalCapitalize } from "@/app/utils/optionalCapitalize";
-
-export function formatCoinName(name: string, symbol: string) {
-  return `${optionalCapitalize(name)} (${symbol.toUpperCase()})`;
-}
--- app/utils/optionalCapitalize.ts
+++
@@ -1,5 +0,0 @@
-export function optionalCapitalize(string: string | undefined) {
-  if (string) {
-    return string?.charAt(0).toUpperCase() + string?.slice(1);
-  };
-};

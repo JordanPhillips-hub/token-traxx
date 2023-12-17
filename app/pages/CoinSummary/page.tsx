2306b6e0062314d09d59475d8e64534b8a06dba7
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-17 09:46:54 -0600

    Setup coin page structure

--- app/pages/CoinSummary/page.tsx
+++ app/pages/CoinSummary/page.tsx
@@ -1,15 +1,35 @@
-import CardHeader from "./CardHeader";
+"use client";
+import { Provider } from "react-redux";
+import MainCard from "./MainCard";
+import { store } from "@/app/store/store";
+import { useAppSelector } from "@/app/store/hooks";
+import { useGetCoinQuery } from "@/app/store/api/coingecko";
 
 export default function CoinSummary() {
+  const { coinSummaryId } = useAppSelector((state) => state.activeLink);
+  const { data: coinInfo, isError } = useGetCoinQuery({ id: coinSummaryId });
+
+  if (coinInfo) {
+    console.log(coinInfo);
+  }
+
   return (
-    <main>
-      <section>
-        <CardHeader />
-      </section>
+    <Provider store={store}>
+      {coinInfo && (
+        <main className="container mx-auto">
+          <section>
+            <MainCard
+              header={`${coinInfo.name} (${coinInfo.symbol.toUpperCase()})`}
+              site={coinInfo.links.homepage[0]}
+              image={coinInfo.image.small}
+            />
+          </section>
 
-      <hr />
+          <hr />
 
-      <section></section>
-    </main>
+          <section></section>
+        </main>
+      )}
+    </Provider>
   );
 }

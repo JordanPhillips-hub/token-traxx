e2d5cf9c946063918b06054ea1112b1d73a797f5
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-17 10:57:23 -0600

    Refactor main card
    Removed header and types and updated with the header component

--- app/pages/CoinSummary/MainCard.tsx
+++ app/pages/CoinSummary/MainCard.tsx
@@ -1,30 +1,10 @@
-import Image from "next/image";
 import Card from "@/app/components/UI/Card";
-import Icon from "@/app/components/UI/Icon";
+import CardHeader from "./CardHeader";
 
-interface MainCardProps {
-  image: string;
-  header: string;
-  site: string;
-}
-
-export default function MainCard({ image, header, site }: MainCardProps) {
+export default function MainCard() {
   return (
     <Card>
-      <header>
-        <div>
-          <div className="inline-block mr-6">
-            <Image src={image} alt="Coin" width={48} height={48} />
-          </div>
-          <div className="inline-block">
-            <h1 className="text-2xl	font-bold">{header}</h1>
-            <p className="font-medium flex items-center gap-2">
-              <span>{site}</span>
-              <Icon iconVariant="copy" />
-            </p>
-          </div>
-        </div>
-      </header>
+      <CardHeader />
 
       <hr />
 

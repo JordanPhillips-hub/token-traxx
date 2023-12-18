d8e6a118312b67ad3f574d2fbeaf294cb2b337f5
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-18 09:37:50 -0600

    Create all time overview

---
+++ app/components/UI/AllTimeOverview.tsx
@@ -0,0 +1,48 @@
+import Icon from "./Icon";
+import { useAppSelector } from "@/app/store/hooks";
+import { formatPrice } from "@/app/utils/numberFormatting";
+import { formatDateString } from "@/app/utils/dateAndTime";
+
+type AllTimeOverviewProps = { allTime: string };
+
+export default function AllTimeOverview({ allTime }: AllTimeOverviewProps) {
+  const { summaryCoin } = useAppSelector((state) => state.coinSummary);
+  const {
+    ath,
+    atl,
+    ath_date: athDate,
+    atl_date: atlDate,
+  } = summaryCoin.market_data;
+
+  const allTimeIsHigh = allTime === "high";
+
+  const iconClass = allTimeIsHigh
+    ? "text-accent100"
+    : "text-accent200 rotate-180";
+
+  return (
+    <div className="mt-7">
+      <div className="flex items-center justify-between">
+        <div>
+          <div className="flex items-center">
+            <Icon className={iconClass} iconVariant="triangle" />
+            <p className="ml-4">All time {allTime}:</p>
+          </div>
+          <p className="ml-8">
+            {allTimeIsHigh
+              ? formatDateString(athDate.usd)
+              : formatDateString(atlDate.usd)}
+          </p>
+        </div>
+
+        <div>
+          <span>
+            {allTimeIsHigh
+              ? `$${formatPrice(ath.usd)}`
+              : `$${formatPrice(atl.usd)}`}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}

9b647789a2f303e57e03672aaa4a5debfdf7b615
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-18 10:12:09 -0600

    Refactor CardHeader
    Created variables to hold state allowing for cleaner more maintainable code

--- app/pages/CoinSummary/CardHeader.tsx
+++ app/pages/CoinSummary/CardHeader.tsx
@@ -4,25 +4,26 @@ import Icon from "@/app/components/UI/Icon";
 
 export default function CardHeader() {
   const { summaryCoin } = useAppSelector((state) => state.coinSummary);
+  const { name, symbol } = summaryCoin;
+  const { small: coinImage } = summaryCoin.image;
+  const { homepage: copyLink } = summaryCoin.links;
+  const formattedLink = copyLink[0]
+    .replace(/^https?:\/\//, "")
+    .replace(/\//g, "");
 
   return (
     <header>
       <div>
         <div className="inline-block mr-6">
-          <Image
-            src={summaryCoin.image.small}
-            alt={`${summaryCoin.name} icon`}
-            width={48}
-            height={48}
-          />
+          <Image src={coinImage} alt={`${name} icon`} width={48} height={48} />
         </div>
         <div className="inline-block">
-          <h1 className="text-2xl	font-bold">{`${
-            summaryCoin.name
-          } (${summaryCoin.symbol.toUpperCase()})`}</h1>
+          <h1 className="text-2xl	font-bold">{`${name} (${symbol.toUpperCase()})`}</h1>
           <p className="font-medium flex items-center gap-2">
-            <span>{summaryCoin.links.homepage[0]}</span>
-            <Icon iconVariant="copy" />
+            <span>{formattedLink}</span>
+            <button>
+              <Icon iconVariant="copy" />
+            </button>
           </p>
         </div>
       </div>

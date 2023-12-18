b85cc37b7d845a789aa27cf3e23abb7244abc704
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-18 10:12:33 -0600

    Impoer new components to main card

--- app/pages/CoinSummary/MainCard.tsx
+++ app/pages/CoinSummary/MainCard.tsx
@@ -1,16 +1,16 @@
-import Card from "@/app/components/UI/Card";
 import CardHeader from "./CardHeader";
+import Card from "@/app/components/UI/Card";
+import PriceOverview from "@/app/components/UI/PriceOverview";
+import AllTimeOverview from "@/app/components/UI/AllTimeOverview";
 
 export default function MainCard() {
   return (
     <Card>
       <CardHeader />
-
-      <hr />
-
-      <div></div>
-
-      <div></div>
+      <PriceOverview />
+      <hr className="bg-neutral50 border-neutral50" />
+      <AllTimeOverview allTime="high" />
+      <AllTimeOverview allTime="low" />
     </Card>
   );
 }

6052a10d18bc68b0502f9607aaed6e1d9b391f04
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-18 20:43:39 -0600

    Create function to copy text to clipboard

--- app/utils/generalHelpers.ts
+++ app/utils/generalHelpers.ts
@@ -6,4 +6,8 @@ export function optionalCapitalize(string: string | undefined) {
 
 export function formatCoinName(name: string, symbol: string) {
   return `${optionalCapitalize(name)} (${symbol.toUpperCase()})`;
-}
+};
+
+export function copyToClipboard(text: string) {
+  navigator.clipboard.writeText(text);
+};

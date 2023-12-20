97cec18a8c8ae97ab1f5b7a608b05a01308b2b2a
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-20 11:52:18 -0600

    Remove console log

--- app/pages/CoinSummary/page.tsx
+++ app/pages/CoinSummary/page.tsx
@@ -53,8 +53,6 @@ export default function CoinSummary() {
     setReadMore(!readMore);
   }
 
-  console.log(summaryCoin);
-
   return (
     <Provider store={store}>
       {isLoading && <p className="text-xl text-center">Loading...</p>}

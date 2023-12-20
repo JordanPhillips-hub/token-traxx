6c6e9d6da062dda60c5f0b3d1acfea50673f5ec5
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-20 11:41:45 -0600

    Create EllipsePlusIcon component

---
+++ app/components/UI/EllipsePlusIcon.tsx
@@ -0,0 +1,9 @@
+import Icon from "./Icon";
+
+export default function EllipsePlusIcon() {
+  return (
+    <div className="bg-primary500 flex items-center justify-center w-6 h-6 rounded-full shadow-neutral50 shadow-sm border border-neutral50">
+      <Icon iconVariant="plus" />
+    </div>
+  );
+}

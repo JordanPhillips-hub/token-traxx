80fba1a6ecfe4829a77149364270551e0a1b83a5
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-20 11:45:09 -0600

    Refactor page
    Updated the page to include the stat cards and the read more functionality for the description

--- app/pages/CoinSummary/page.tsx
+++ app/pages/CoinSummary/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import MainCard from "./MainCard";
+import StatCard from "./StatCard";
 import { store } from "@/app/store/store";
 import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import { useGetCoinQuery } from "@/app/store/api/coingecko";
@@ -11,11 +12,20 @@ import CopyButton from "@/app/components/UI/Buttons/CopyButton";
 
 export default function CoinSummary() {
   const dispatch = useAppDispatch();
+  const [readMore, setReadMore] = useState<boolean>(false);
   const { coinSummaryId } = useAppSelector((state) => state.activeLink);
   const { summaryCoin } = useAppSelector((state) => state.coinSummary);
   const { description } = summaryCoin;
   const { blockchain_site: siteLink } = summaryCoin.links;
-  const siteLinks = [siteLink[0], siteLink[1], siteLink[2]];
+  const {
+    total_volume: volume,
+    mcap_to_tvl_ratio: ratio,
+    market_cap: cap,
+    market_cap_change_24h: changeIn24h,
+    fully_diluted_valuation: dilutedVal,
+    max_supply: max,
+    circulating_supply: circulating,
+  } = summaryCoin.market_data;
 
   const {
     data: coinInfo,
@@ -23,11 +33,27 @@ export default function CoinSummary() {
     isError,
   } = useGetCoinQuery({ id: coinSummaryId });
 
+  const siteLinks = [siteLink[0], siteLink[1], siteLink[2]];
+  const percentage = (circulating / max) * 100;
+  const volumeStats = { "Total Volume": volume.usd, "Volume/Market": ratio };
+  const SupplyStats = { "Max Supply": max, "Circulating Supply": circulating };
+  const marketStats = {
+    "Market Cap Change": changeIn24h,
+    "Market Cap": cap.usd,
+    "Fully Diluted Valuation": dilutedVal.usd,
+  };
+
   useEffect(() => {
     if (coinInfo) {
       dispatch(setSummaryCoin(coinInfo));
     }
   });
+
+  function toggleReadMore() {
+    setReadMore(!readMore);
+  }
+
+  console.log(summaryCoin);
 
   return (
     <Provider store={store}>
@@ -43,7 +69,20 @@ export default function CoinSummary() {
             </section>
 
             <section>
-              <p>{description.en}</p>
+              <div
+                className={
+                  !readMore
+                    ? "overflow-hidden max-h-48"
+                    : "overflow-visible max-h-full"
+                }
+              >
+                <p>{description.en}</p>
+              </div>
+
+              <button className="text-primary500" onClick={toggleReadMore}>{`${
+                !readMore ? "Read More..." : "Read Less..."
+              }`}</button>
+
               <div className="flex flex-wrap gap-6 mt-6">
                 {siteLinks.map((link) => (
                   <Card key={link} className="w-fit rounded-xl py-4 px-6">
@@ -56,7 +95,15 @@ export default function CoinSummary() {
 
           <hr />
 
-          <section></section>
+          <section className="grid grid-cols-2 gap-6 my-8">
+            <StatCard stats={marketStats} hasStatusBar={false} />
+            <StatCard
+              stats={SupplyStats}
+              hasStatusBar={true}
+              completed={percentage}
+            />
+            <StatCard stats={volumeStats} hasStatusBar={false} />
+          </section>
         </main>
       )}
     </Provider>

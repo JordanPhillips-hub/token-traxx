d2775e50c258bd079119481bed20983a533f7006
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-27 12:45:19 -0600

    Create CoinCard component

---
+++ app/pages/Portfolio/AddAsset/CoinCard.tsx
@@ -0,0 +1,22 @@
+import Card from "@/app/components/UI/Card";
+import Image from "next/image";
+import { optionalCapitalize } from "@/app/utils/generalHelpers";
+
+type CoinCardProps = {
+  image: string;
+  name: string;
+  symbol: string;
+};
+
+export function CoinCard({ image, name, symbol }: CoinCardProps) {
+  return (
+    <Card className="flex flex-col items-center justify-center gap-4">
+      <div className="bg-[#2C2C4A] flex items-center justify-center w-1/4 h-1/3 rounded-lg">
+        <Image src={image} alt={`${name} logo`} width={42} height={42} />
+      </div>
+      <p className="text-3xl font-bold">{`${optionalCapitalize(
+        name
+      )} (${symbol.toUpperCase()})`}</p>
+    </Card>
+  );
+}

30432c511c66ffdc8138e81102a2737ded41296d
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-29 12:43:27 -0600

    Remvoe react import

--- app/components/PriceOverview.tsx
+++ app/components/PriceOverview.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useAppSelector } from "@/app/store/hooks";
 import PriceChange from "@/app/components/UI/PriceChange";
 import { formatCurrency } from "@/app/utils/numberFormatting";

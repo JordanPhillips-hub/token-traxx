67564af5ee3249b48353252663e83e64a2b41d96
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-12-31 10:28:32 -0600

    Refactor ConvertorDropdown

--- app/components/CurrencyConvertor/ConvertorDropdown.tsx
+++ app/components/CurrencyConvertor/ConvertorDropdown.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-import Image from "next/image";
+import Dropdown from "@/app/components/UI/Dropdown";
+import CoinName from "@/app/components/UI/CoinName";
 import { useAppSelector, useAppDispatch } from "@/app/store/hooks";
+import { setCoinId } from "@/app/store/features/coinMarketSlice";
+import { setComparedCoins } from "@/app/store/features/charts/compareChartSlice";
 import {
   setSellCoinId,
   setBuyCoinId,
   setNumToSell,
   setSellPrice,
 } from "@/app/store/features/convertorSlice";
-import { setCoinId } from "@/app/store/features/coinMarketSlice";
-import { setComparedCoins } from "@/app/store/features/charts/compareChartSlice";
-import Icon from "@/app/components/UI/Icon";
-import { formatCoinName } from "@/app/utils/generalHelpers";
 
 type DropdownProps = {
   componentType: string;
@@ -23,27 +22,16 @@ type DropdownProps = {
 export default function ConvertorDropdown({
   componentType,
   image,
-  name,
   id,
   symbol,
 }: DropdownProps) {
   const dispatch = useAppDispatch();
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-  const { coins, currency } = useAppSelector((state) => state.coinMarkets);
   const { comparedCoins } = useAppSelector((state) => state.compareCharts);
+  const { coins } = useAppSelector((state) => state.coinMarkets);
 
   function handleDropdown() {
     setIsDropdownOpen(!isDropdownOpen);
-  }
-
-  function handleCoinSelect(id: string) {
-    componentType === "sell"
-      ? dispatch(setSellCoinId(id))
-      : dispatch(setBuyCoinId(id));
-
-    dispatch(setNumToSell(0));
-    dispatch(setSellPrice(0));
-    setIsDropdownOpen(false);
   }
 
   function handleCoinComparison(id: string) {
@@ -55,41 +43,39 @@ export default function ConvertorDropdown({
     }
   }
 
-  return (
-    <div className="relative">
-      <button
-        className="text-xl font-medium flex items-center gap-2 mb-4"
-        onClick={handleDropdown}
-      >
-        <Image src={image} alt={name} width={30} height={30} />
-        <p>{formatCoinName(id ?? "", symbol ?? "")}</p>
-        <div className={`${isDropdownOpen ? "rotate-180" : ""}`}>
-          <Icon iconVariant="chevDown" />
-        </div>
-        {currency.toUpperCase()}
-      </button>
+  function handleCoinSelect(id: string) {
+    componentType === "sell"
+      ? dispatch(setSellCoinId(id))
+      : dispatch(setBuyCoinId(id));
 
-      <div
-        className={`${
-          isDropdownOpen
-            ? "text-sm absolute bg-blue600 mt-3 px-4 rounded"
-            : "hidden"
-        }`}
-      >
-        {coins.map((coin) => (
-          <button
-            key={coin.id}
-            className="flex items-center gap-2 mb-3 hover:text-base hover:font-medium"
-            onClick={() => {
-              handleCoinSelect(coin.id);
-              handleCoinComparison(coin.id);
-            }}
-          >
-            <Image src={coin.image} alt={coin.name} width={25} height={25} />
-            <p> {formatCoinName(coin.id ?? "", coin.symbol ?? "")}</p>
-          </button>
-        ))}
-      </div>
-    </div>
+    dispatch(setNumToSell(0));
+    dispatch(setSellPrice(0));
+    setIsDropdownOpen(false);
+    handleCoinComparison(id);
+  }
+
+  return (
+    <Dropdown
+      className="text-xl"
+      img={image}
+      imgWidth={30}
+      imgHeight={30}
+      id={id}
+      symbol={symbol}
+      isOpen={isDropdownOpen}
+      onClick={() => handleDropdown()}
+    >
+      {coins.map((coin) => (
+        <button key={coin.id} onClick={() => handleCoinSelect(coin.id)}>
+          <CoinName
+            img={coin.image}
+            imgWidth={25}
+            imgHeight={25}
+            id={coin.id}
+            symbol={coin.symbol}
+          />
+        </button>
+      ))}
+    </Dropdown>
   );
 }

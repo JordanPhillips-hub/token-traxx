0a1b9eee48288117c46237b033fc134a28bac7f0
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-05 11:33:09 -0600

    Update img props to be full image name

--- app/components/UI/CoinName.tsx
+++ app/components/UI/CoinName.tsx
@@ -2,23 +2,28 @@ import Image from "next/image";
 import { formatCoinName } from "@/app/utils/generalHelpers";
 
 type CoinNameProps = {
-  img: string;
-  imgWidth: number;
-  imgHeight: number;
+  image: string;
+  imageWidth: number;
+  imageHeight: number;
   id: string;
   symbol: string;
 };
 
 export default function CoinName({
-  img,
-  imgWidth,
-  imgHeight,
+  image,
+  imageWidth,
+  imageHeight,
   id,
   symbol,
 }: CoinNameProps) {
   return (
     <div className="flex items-center gap-2">
-      <Image src={img} alt={`${id} icon`} width={imgWidth} height={imgHeight} />
+      <Image
+        src={image}
+        alt={`${id} icon`}
+        width={imageWidth}
+        height={imageHeight}
+      />
       <p>{formatCoinName(id ?? "", symbol ?? "")}</p>
     </div>
   );

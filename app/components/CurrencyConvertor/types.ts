b448699484066def208d5a8038b111ebf99008a8
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-05 11:32:43 -0600

    Create new types file for CurrencyConvertor

---
+++ app/components/CurrencyConvertor/types.ts
@@ -0,0 +1,13 @@
+export type DropdownProps = {
+  cardType: string;
+  image: string;
+  name: string;
+  id: string;
+  symbol: string;
+};
+
+export type DropdownItem = {
+  image: string;
+  id: string;
+  symbol: string;
+};

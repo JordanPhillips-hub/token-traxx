e391d1d90f660f900c66e5cd53354ac283ef68d9
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-07 14:13:24 -0600

    Create types file

---
+++ app/components/Navbar/types.ts
@@ -0,0 +1,9 @@
+type CoinInfoProps = {
+  icon?: string;
+  name?: string;
+  data: number | string;
+  hasStatBar?: boolean;
+  completed: number;
+  hasPriceChange?: boolean;
+  changePercent: number;
+};

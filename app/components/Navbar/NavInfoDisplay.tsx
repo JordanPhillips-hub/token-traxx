ec4963baa16485e3397ed870038ecb4ef2f8132e
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-07 13:48:50 -0600

    Refactor NavInfoDisplay
    Removed unnecessary props from CoinInfo components to reduce code
    
    Created variable to hold the formatted market cap

--- app/components/Navbar/NavInfoDisplay.tsx
+++ app/components/Navbar/NavInfoDisplay.tsx
@@ -18,6 +18,10 @@ export default function NavInfoDisplay() {
     market_cap_percentage: marketCapPercent,
   } = data;
 
+  const formattedMarketCap = `$${formatNum3_2(
+    marketCap.btc
+  )} ${checkNumberScale(marketCap.btc)}`;
+
   useEffect(() => {
     if (coinGlobals) {
       dispatch(setCoinGlobals({ data: coinGlobals.data }));
@@ -26,46 +30,19 @@ export default function NavInfoDisplay() {
 
   return (
     <div className="bg-purple700 flex items-center justify-center gap-8 mb-6 px-16 py-4 border-b-[1px] border-white border-opacity-[0.1]">
-      <CoinInfo
-        name="Coins"
-        icon="coin"
-        data={activeCurrencies}
-        completed={0}
-        changePercent={0}
-      />
-      <CoinInfo
-        name="Exchange"
-        icon="exchange"
-        data={markets}
-        completed={0}
-        changePercent={0}
-      />
-      <CoinInfo
-        data=""
-        completed={0}
-        hasPriceChange={true}
-        changePercent={marketCapChangeIn24h}
-      />
-      <CoinInfo
-        data={`$${formatNum3_2(marketCap.btc)} ${checkNumberScale(
-          marketCap.btc
-        )}`}
-        completed={0}
-        changePercent={0}
-      />
+      <CoinInfo name="Coins" icon="coin" data={activeCurrencies} />
+      <CoinInfo name="Exchange" icon="exchange" data={markets} />
+      <CoinInfo changePercent={marketCapChangeIn24h} />
+      <CoinInfo data={formattedMarketCap} />
       <CoinInfo
         name="BTC"
         data={`${Math.floor(marketCapPercent.btc)}%`}
-        hasStatBar={true}
         completed={Math.floor(marketCapPercent.btc)}
-        changePercent={0}
       />
       <CoinInfo
         name="ETH"
         data={`${Math.floor(marketCapPercent.eth)}%`}
-        hasStatBar={true}
         completed={Math.floor(marketCapPercent.eth)}
-        changePercent={0}
       />
     </div>
   );

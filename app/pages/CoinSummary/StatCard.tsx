87f7abc9c5eb2bf3afcbf6275c07aa8a1b7a2e01
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-12 11:49:37 -0600

    Create variable for coin stats and stat display value

--- app/pages/CoinSummary/StatCard.tsx
+++ app/pages/CoinSummary/StatCard.tsx
@@ -14,18 +14,21 @@ export default function StatCard({
   hasStatusBar,
   completed,
 }: StatCardProps) {
+  const coinStats = Object.entries(stats).map(([name, stat]) => (
+    <CoinStat key={name} statName={name} stat={formatCurrency(stat)} />
+  ));
+
+  const statDisplayValue =
+    completed && isFinite(completed) ? Math.floor(completed) + "%" : "N/A";
+
   return (
     <Card className="py-10 px-8">
       <div className="flex flex-col gap-8">
-        {Object.entries(stats).map(([name, stat]) => (
-          <CoinStat key={name} statName={name} stat={formatCurrency(stat)} />
-        ))}
+        {coinStats}
         {hasStatusBar && completed !== undefined && (
           <div className="relative mt-8">
             <p className="text-orange500 text-xs absolute bottom-2 left-0">
-              {completed && isFinite(completed)
-                ? Math.floor(completed) + "%"
-                : "N/A"}
+              {statDisplayValue}
             </p>
             <p className="text-xs absolute bottom-2 right-0">100%</p>
             <ProgressBar

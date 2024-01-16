46b41b2b36f4264923fca954eef1c65f0e8a38c2
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-16 11:17:42 -0600

    Remove form tag / Import FormInput component

--- app/components/CurrencyConvertor/SellInput.tsx
+++ app/components/CurrencyConvertor/SellInput.tsx
@@ -1,19 +1,16 @@
-import React, { ChangeEvent, FormEvent } from "react";
+import React, { ChangeEvent } from "react";
 import { useGetSelectedCoin } from "./hooks";
+import FormInput from "@/app/components/Form/FormInput";
 import { useAppSelector, useAppDispatch } from "@/app/store/hooks";
 import {
   setNumToSell,
   setSellPrice,
 } from "@/app/store/features/convertorSlice";
 
-export default function SellForm({ cardType }: { cardType: string }) {
+export default function SellInput({ cardType }: { cardType: string }) {
   const dispatch = useAppDispatch();
   const { numToSell } = useAppSelector((state) => state.convertor);
   const currentPrice = useGetSelectedCoin(cardType)?.current_price;
-
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-  }
 
   function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
     const inputValue = parseFloat(e.target.value);
@@ -23,16 +20,15 @@ export default function SellForm({ cardType }: { cardType: string }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className="p-2 border border-gray-300 rounded w-full focus:outline-none focus:border-purple500"
-        type="number"
-        name="toSell"
-        id="toSell"
-        onChange={handleInputChange}
-        value={numToSell}
-        min={1}
-      />
-    </form>
+    <FormInput
+      className="bg-transparent p-2"
+      label="You Sell"
+      id="toSell"
+      name="toSell"
+      type="number"
+      value={numToSell}
+      min={1}
+      onChange={handleInputChange}
+    />
   );
 }

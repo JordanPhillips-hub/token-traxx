f9d4167109fbe0b8520a1b115d9f92af3918b01e
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-01-15 21:54:18 -0600

    Update CoinName props

--- app/components/UI/CoinName.tsx
+++ app/components/UI/CoinName.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import { formatCoinName } from "@/app/utils/generalHelpers";
 
 type CoinNameProps = {
+  hasImage?: boolean;
   image: string;
-  imageWidth: number;
-  imageHeight: number;
+  imageWidth?: number;
+  imageHeight?: number;
   id: string;
   symbol: string;
 };
 
 export default function CoinName({
+  hasImage,
   image,
   imageWidth,
   imageHeight,
@@ -18,12 +20,14 @@ export default function CoinName({
 }: CoinNameProps) {
   return (
     <div className="flex items-center gap-2">
-      <Image
-        src={image}
-        alt={`${id} icon`}
-        width={imageWidth}
-        height={imageHeight}
-      />
+      {hasImage && (
+        <Image
+          src={image}
+          alt={`${id} icon`}
+          width={imageWidth}
+          height={imageHeight}
+        />
+      )}
       <p>{formatCoinName(id ?? "", symbol ?? "")}</p>
     </div>
   );

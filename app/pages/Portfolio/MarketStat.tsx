f8f38c8b3849ba4a656163f73dd267b2cb34b24a
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-02-01 07:28:14 -0600

    Replace ProgressBar with StatBar component

--- app/pages/Portfolio/MarketStat.tsx
+++ app/pages/Portfolio/MarketStat.tsx
@@ -1,5 +1,5 @@
-import ProgressBar from "@ramonak/react-progress-bar";
 import PriceChange from "@/app/components/UI/PriceChange";
+import StatBar from "@/app/components/UI/StatBar";
 
 type MarketStatProps = {
   stat: string;
@@ -23,9 +23,7 @@ export default function MarketStat({
   return (
     <div className="py-3 px-6 border border-solid border-[#2d2d54] rounded-lg">
       {!percentChange && !hasStatusBar && <p>{statValue}</p>}
-      {percentChange && (
-        <PriceChange className="text-base" percentage={percentage} />
-      )}
+      {percentChange && (<PriceChange className="text-base" percentage={percentage} />)}
       {hasStatusBar && completed !== undefined && (
         <div className="relative mt-8">
           <p className="text-green500 text-xs absolute bottom-2 left-0">
@@ -33,14 +31,7 @@ export default function MarketStat({
               ? Math.floor(completed) + "%"
               : "N/A"}
           </p>
-          <ProgressBar
-            completed={completed}
-            animateOnRender={true}
-            labelColor="transparent"
-            baseBgColor="hsla(0, 0%, 100%, 0.5)"
-            bgColor="hsl(176, 100%, 48%)"
-            height="5px"
-          />
+          <StatBar completed={completed} bgColor="hsl(176, 100%, 48%)"/>
         </div>
       )}
       <p className="text-sm mt-2">{stat}</p>

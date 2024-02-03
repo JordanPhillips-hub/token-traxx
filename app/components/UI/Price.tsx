f62ec34f251caf177cf5365e852d487f07ed20cd
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-02-03 13:48:32 -0600

    Create classname prop

--- app/components/UI/Price.tsx
+++ app/components/UI/Price.tsx
@@ -4,15 +4,16 @@ import { formatCurrency } from "@/app/utils/numberFormatting";
 type PriceProps = {
   price: number;
   hasCode: boolean;
+  className?: string;
 };
 
-export default function Price({ price, hasCode }: PriceProps) {
+export default function Price({ price, hasCode, className }: PriceProps) {
   const { currency, currencySymbol } = useAppSelector(
     (state) => state.coinMarkets
   );
 
   return (
-    <p>
+    <p className={className}>
       {hasCode
         ? `${currencySymbol}${formatCurrency(price)} ${currency.toUpperCase()}`
         : `${currencySymbol}${formatCurrency(price)}`}

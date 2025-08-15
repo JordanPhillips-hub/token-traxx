e9dc6aa1a45925f1ed86060de8365cbef7419755
JordanPhillips-hub <jordan.phillips.88@outlook.com>
2025-08-15 08:46:23 -0600

    Fix convertor close button light dark mode colors

--- app/components/UI/Buttons/CloseButton.tsx
+++ app/components/UI/Buttons/CloseButton.tsx
@@ -8,10 +8,10 @@ type CloseButtonProps = {
 
 export default function CloseButton({ onClose, className }: CloseButtonProps) {
   const defaultStyles =
-    "bg-blue700 focus:bg-purple500 hover:bg-purple500 px-2 py-1 rounded";
+    "bg-blue700 focus:bg-purple500 hover:bg-purple500 px-2 py-1 rounded dark:bg-blue700 dark:hover:bg-purple500 dark:focus:bg-purple500";
   return (
     <button className={twMerge(defaultStyles, className)} onClick={onClose}>
-      <div className="flex items-center gap-1">
+      <div className="text-white flex items-center gap-1">
         <Icon iconVariant="exit" />
         <p>Close</p>
       </div>

6d1912146b8f46e37959749e1a50becc19e75e46
JordanPhillips-hub <jordan.phillips.88@outlook.com>
2025-08-17 09:08:55 -0600

    Update CoinCard containers and text with light / dark mode colors

--- app/pages/Portfolio/AddAsset/CoinCard.tsx
+++ app/pages/Portfolio/AddAsset/CoinCard.tsx
@@ -5,11 +5,13 @@ import { optionalCapitalize } from "@/app/utils/generalHelpers";
 
 export function CoinCard({ image, name, symbol }: CoinCardProps) {
   return (
-    <Card className="flex flex-col items-center justify-center gap-4">
-      <div className="bg-[#2C2C4A] flex items-center justify-center w-1/4 h-1/3 rounded-lg">
+    <Card className="bg-white dark:bg-blue900 flex flex-col items-center justify-center gap-4">
+      <div className="flex items-center justify-center w-1/4 h-1/3 rounded-lg">
         <Image src={image} alt={`${name} logo`} width={42} height={42} />
       </div>
-      <p className="text-3xl font-bold">{`${optionalCapitalize(name)} (${symbol.toUpperCase()})`}</p>
+      <p className="text-3xl font-bold">{`${optionalCapitalize(
+        name
+      )} (${symbol.toUpperCase()})`}</p>
     </Card>
   );
 }
